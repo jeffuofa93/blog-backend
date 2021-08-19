266b8bd91e7bd21a4e0cbd786aa45ed80ed8fa36
jeffuofa93 <wiederkehr@email.arizona.edu>
2021-08-19 13:19:47 -0700

    adjusted the populate query to include the username and name of the user instead of just the id for update and get by id

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,36 +18,16 @@ blogRouter.get("/", async (request, response) => {
 });
 
 blogRouter.get("/:id", async (request, response) => {
-  const blog = await Blog.findById(request.params.id);
+  const blog = await Blog.findById(request.params.id).populate("user", {
+    username: 1,
+    name: 1,
+  });
   if (blog) {
     response.json(blog);
   } else {
     response.status(404).end();
   }
 });
-
-// blogRouter.post("/", async (request, response) => {
-//   const body = request.body;
-//   const token = getTokenFrom(request);
-//   const decodedToken = jwt.verify(token, process.env.SECRET);
-//   if (!token || !decodedToken.id) {
-//     return response.status(401).json({ error: "token missing or invalid" });
-//   }
-//   const user = await User.findById(decodedToken.id);
-//
-//   const blog = new Blog({
-//     title: body.title,
-//     author: body.author,
-//     url: body.url,
-//     likes: body.likes,
-//     user: user._id,
-//   });
-//
-//   const savedBlog = await blog.save();
-//   user.blogs = user.blogs.concat(savedBlog._id);
-//   await user.save();
-//   response.json(savedBlog);
-// });
 
 blogRouter.post("/", async (request, response) => {
   const body = request.body;
@@ -69,15 +49,13 @@ blogRouter.post("/", async (request, response) => {
   const savedBlog = await blog.save();
   user.blogs = user.blogs.concat(savedBlog._id);
   await user.save();
+  console.log(savedBlog);
   response.json(savedBlog);
 });
 
 blogRouter.delete("/:id", async (request, response) => {
   const removeBlog = await Blog.findById(request.params.id);
   const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  // const testUser = request.user;
-  // logger.info(testUser);
-  // return;
   if (!removeBlog || !decodedToken.id) {
     return response.status(401).json({ error: "token error or user error" });
   }
@@ -104,7 +82,8 @@ blogRouter.put("/:id", async (request, response) => {
     new: true,
     runValidators: true,
     context: "query",
-  });
+  }).populate("user", { username: 1, name: 1 });
+
   response.json(updatedBlog);
 });
 
